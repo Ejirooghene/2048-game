e34563cb8f40ba6f0e3dca96b8a1492a67e15cc8
Joshua Ejiro <joshuaejiro58@gmail.com>
2023-10-03 05:05:23 +0100

    first commit

---
+++ Tile.js
@@ -0,0 +1,50 @@
+export default class Tile {
+    #tileElement
+    #x
+    #y
+    #value
+
+    constructor(tileContainer, value = Math.random() > .4 ? 2 : 4){
+        this.#tileElement = document.createElement('div');
+        this.#tileElement.classList.add('tile');
+        tileContainer.append(this.#tileElement);
+        this.value = value;
+    }
+    
+    get value(){
+        return this.#value; 
+    }
+
+    set value(v) { 
+        this.#value = v;
+        this.#tileElement.textContent = v;  
+        const power = Math.log2(v);
+        switch(power) {
+            case 1:
+                this.#tileElement.style.backgroundColor = '#ffff80';
+                break;
+            case 2:
+                this.#tileElement.style.backgroundColor = '#ffd966';
+        }
+    }
+
+    set x(value){
+        this.#x = value;
+        this.#tileElement.style.setProperty('--x', value);
+    }
+
+    set y(value){
+        // this.#y = value;
+        this.#tileElement.style.setProperty('--y', value);
+    }
+
+    remove(){
+        this.#tileElement.remove();
+    }
+
+    waitForTransition(animation = false){
+        return new Promise(resove => {
+            this.#tileElement.addEventListener( animation ? 'animationend' : 'transitionend', resolve, { once: true })
+        })
+    }
+}     
